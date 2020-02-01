0705cf0454f91e7abfe1e8a0b322e932fc020248
paulwinter <pwinter303@yahoo.com>
2020-02-01 11:49:11 -0500

    Added Pages

---
+++ resources/js/components/UserStockCreate.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class UserStockCreate extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">User Stock Create</div>
+
+                            <div className="card-body">
+                                Placeholder until the real page is done.....
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
